56d547ce100a57238969b6e264d9d2d2cf755b66
Alexandra Berke <berke.alexandra@gmail.com>
2018-11-12 12:09:28 -0500

    only using cactus scnassets -- cactus is spinning at slightly too large size

--- cacti-mirror/ViewController.swift
+++ cacti-mirror/ViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Alexandra Berke on 11/11/18.
 //  Copyright © 2018 aberke. All rights reserved.
 //
+// Refer to:
+// https://developer.apple.com/documentation/arkit/building_your_first_ar_experience
 
 import UIKit
 import SceneKit
@@ -18,7 +20,7 @@ class ViewController: UIViewController {
     
     let fadeDuration: TimeInterval = 0.3
     let rotateDuration: TimeInterval = 3
-    let waitDuration: TimeInterval = 0.5
+    let waitDuration: TimeInterval = 5
     
     lazy var fadeAndSpinAction: SCNAction = {
         return .sequence([
@@ -37,29 +39,22 @@ class ViewController: UIViewController {
             ])
     }()
     
+    // Load the objects that will be used as overlay nodes.
     lazy var treeNode: SCNNode = {
         guard let scene = SCNScene(named: "tree.scn"),
             let node = scene.rootNode.childNode(withName: "tree", recursively: false) else { return SCNNode() }
         let scaleFactor = 0.005
         node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        // Rotate the object to match the orientation of the node it is overlayed on to.
         node.eulerAngles.x = -.pi / 2
         return node
     }()
     
-    lazy var bookNode: SCNNode = {
-        guard let scene = SCNScene(named: "book.scn"),
-            let node = scene.rootNode.childNode(withName: "book", recursively: false) else { return SCNNode() }
-        let scaleFactor  = 0.1
+    lazy var cactusNode: SCNNode = {
+        guard let scene = SCNScene(named: "art.scnassets/cactus.scn"),
+            let node = scene.rootNode.childNode(withName: "cactus", recursively: false) else { return SCNNode() }
+        let scaleFactor = 0.002
         node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
-        return node
-    }()
-    
-    lazy var mountainNode: SCNNode = {
-        guard let scene = SCNScene(named: "mountain.scn"),
-            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
-        let scaleFactor  = 0.25
-        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
-        node.eulerAngles.x += -.pi / 2
         return node
     }()
     
@@ -89,6 +84,9 @@ class ViewController: UIViewController {
     }
     
     func resetTrackingConfiguration() {
+        // This is the function that sets of the AR configuration.
+        // ARWorldTrackingConfiguration class provides motion tracking and enables
+        // features to help you place virtual content in relation to real-world surfaces.
         guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
         let configuration = ARWorldTrackingConfiguration()
         configuration.detectionImages = referenceImages
@@ -100,14 +98,19 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: ARSCNViewDelegate {
-    
+    // By default, the ARSCNView class adds an SCNNode object to the SceneKit scene for each anchor.
+    // Your view’s delegate can implement the renderer(_:didAdd:for:) method to add content
+    // to the scene.
+    // When you add content as a child of the node corresponding to the anchor,
+    // the ARSCNView class automatically moves that content as ARKit refines its
+    // estimate of the plane’s position.
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let imageAnchor = anchor as? ARImageAnchor else { return }
         let referenceImage = imageAnchor.referenceImage
         let imageName = referenceImage.name ?? "no name"
         
         let overlayNode = self.getNode(withImageName: imageName)
-        overlayNode.opacity = 0
+        overlayNode.opacity = 1 //0
         overlayNode.position.y = 0.2
         overlayNode.runAction(self.fadeAndSpinAction)
         
@@ -129,7 +132,7 @@ extension ViewController: ARSCNViewDelegate {
         var node = SCNNode()
         switch name {
         case "lego-cactus-board-for-ar":
-            node = treeNode
+            node = cactusNode
         case "lego-cactus-board-top":
             node = treeNode
         default:

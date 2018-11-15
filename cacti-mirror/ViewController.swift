f7265ade16f75f743adb8df26d8e84c2de4c1c35
Alexandra Berke <berke.alexandra@gmail.com>
2018-11-15 18:45:45 -0500

    cactus shows and is properly positioned for simple board recognition

--- cacti-mirror/ViewController.swift
+++ cacti-mirror/ViewController.swift
@@ -20,43 +20,27 @@ class ViewController: UIViewController {
     
     let fadeDuration: TimeInterval = 0.3
     let rotateDuration: TimeInterval = 3
-    let waitDuration: TimeInterval = 5
     
     lazy var fadeAndSpinAction: SCNAction = {
         return .sequence([
             .fadeIn(duration: fadeDuration),
+            .wait(duration: 3),
             .rotateBy(x: 0, y: 0, z: CGFloat.pi * 360 / 180, duration: rotateDuration),
-            .wait(duration: waitDuration),
-            .fadeOut(duration: fadeDuration)
-            ])
+            .wait(duration: 3),
+            .fadeOut(duration: fadeDuration),
+        ])
     }()
     
-    lazy var fadeAction: SCNAction = {
+    var imageHighlightAction: SCNAction {
         return .sequence([
-            .fadeOpacity(by: 0.8, duration: fadeDuration),
-            .wait(duration: waitDuration),
-            .fadeOut(duration: fadeDuration)
-            ])
-    }()
-    
-    // Load the objects that will be used as overlay nodes.
-    lazy var treeNode: SCNNode = {
-        guard let scene = SCNScene(named: "tree.scn"),
-            let node = scene.rootNode.childNode(withName: "tree", recursively: false) else { return SCNNode() }
-        let scaleFactor = 0.005
-        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
-        // Rotate the object to match the orientation of the node it is overlayed on to.
-        node.eulerAngles.x = -.pi / 2
-        return node
-    }()
-    
-    lazy var cactusNode: SCNNode = {
-        guard let scene = SCNScene(named: "art.scnassets/cactus.scn"),
-            let node = scene.rootNode.childNode(withName: "cactus", recursively: false) else { return SCNNode() }
-        let scaleFactor = 0.002
-        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
-        return node
-    }()
+            .wait(duration: 0.25),
+            .fadeOpacity(to: 0.85, duration: 0.25),
+            .fadeOpacity(to: 0.15, duration: 0.25),
+            .fadeOpacity(to: 0.85, duration: 0.25),
+            .fadeOut(duration: 3),
+            .removeFromParentNode()
+        ])
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -105,40 +89,44 @@ extension ViewController: ARSCNViewDelegate {
     // the ARSCNView class automatically moves that content as ARKit refines its
     // estimate of the plane’s position.
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Get the anchor
         guard let imageAnchor = anchor as? ARImageAnchor else { return }
         let referenceImage = imageAnchor.referenceImage
-        let imageName = referenceImage.name ?? "no name"
         
-        let overlayNode = self.getNode(withImageName: imageName)
-        overlayNode.opacity = 1 //0
-        overlayNode.position.y = 0.2
-        overlayNode.runAction(self.fadeAndSpinAction)
+        // ----- Debugging plane ---------------
+        let debugPlaneNode = getPlaneNode(withReferenceImage: referenceImage)
+        node.addChildNode(debugPlaneNode)
+        // ----- Debugging plane ---------------
         
-        node.addChildNode(overlayNode)
+        let scene = SCNScene(named: "art.scnassets/cactus.scn")!
+        let cactusNode = scene.rootNode.childNode(withName: "cactus", recursively: true)!
+        let scaleFactor = 0.012
+        cactusNode.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        cactusNode.opacity = 1
+        cactusNode.runAction(self.fadeAndSpinAction)
+
+        // Add the cactus node to the scene
+        node.addChildNode(cactusNode)
+        
+        // TODO: follow the image anchor smoothly as it moves
+        // https://hackernoon.com/arkit-tutorial-image-recognition-and-virtual-content-transform-91484ceaf5d5
         
         DispatchQueue.main.async {
-            self.label.text = "cacti detected (\"\(imageName)\")"
+            self.label.text = "cacti detected"
         }
     }
     
     func getPlaneNode(withReferenceImage image: ARReferenceImage) -> SCNNode {
+        // Create a plane to visualize the initial position of the detected image.
         let plane = SCNPlane(width: image.physicalSize.width,
                              height: image.physicalSize.height)
         let node = SCNNode(geometry: plane)
+        node.opacity = 0.25
+        //`SCNPlane` is vertically oriented in its local coordinate space, but
+        //`ARImageAnchor` assumes the image is horizontal in its local space, so
+        //rotate the plane to match.
+        node.eulerAngles.x = -.pi / 2
+        node.runAction(self.imageHighlightAction)
         return node
     }
-    
-    func getNode(withImageName name: String) -> SCNNode {
-        var node = SCNNode()
-        switch name {
-        case "lego-cactus-board-for-ar":
-            node = cactusNode
-        case "lego-cactus-board-top":
-            node = treeNode
-        default:
-            break
-        }
-        return node
-    }
-    
 }
